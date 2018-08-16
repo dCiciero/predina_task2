d748f561a12d70b081532da37b8c6e478556a5e5
Ogaga Ivhurie <ivhurie01@yahoo.co.uk>
2018-08-16 15:15:15 +0100

    display coords for 1 car

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var access_token = "pk.eyJ1Ijoib2djaWNlcm8iLCJhIjoiY2prazk2bDFlMTloeDN2cGs0OWdnZ3M3dCJ9.TjHJEC7jposIe4SV254wJA"
-var map = L.map('map').setView([51.5655555556,-0.303027777778], 8);    
+var map = L.map('map').setView([53.186288,-1.779785], 7);    
 
 L.tileLayer(`https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${access_token}`, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -14,9 +14,9 @@ var carIcon = L.icon({
     popupAnchor:  [-3, -76] 
 });
 
-let time = "";
+let time = "", vehicle = "Vehicle_847" ;
 let coordinates = [], display = [];
-let currentHour=5, currentMinute=0, currentDisplay = 0, maxDiaplay = 20;
+let currentHour=14, currentMinute=52, currentDisplay = 0, maxDiaplay = 1;
 let group = L.layerGroup();
 let marker;
 
@@ -47,21 +47,26 @@ function loadCoord() {
             if (data.hasOwnProperty(key)) {
                 const element = data[key];
                 if (element == time) {
-                    
-                    while (currentDisplay < maxDiaplay) {
-                        coordinates[0] = data.Time;
-                        coordinates[1] = data.Vehicle;
-                        coordinates[2] = parseFloat(data.Latitude);
-                        coordinates[3] = parseFloat(data.Longitude);
-                        displayCoord(coordinates);
-                        currentDisplay++;
-                        break;
+                    if (data.Vehicle == vehicle) {
+                        while (currentDisplay < maxDiaplay) {
+                            coordinates[0] = data.Time;
+                            coordinates[1] = data.Vehicle;
+                            coordinates[2] = parseFloat(data.Latitude);
+                            coordinates[3] = parseFloat(data.Longitude);
+                            displayCoord(coordinates);
+                            currentDisplay++;
+                            break;
+                        }
                     }
+                   
                 } 
             }
         }
+        
+       
     });
 
+    
     
     group.eachLayer(layer => {
         group.removeLayer(layer);
@@ -78,4 +83,3 @@ setInterval(() => {
     loadCoord();
     
 },60000)
-
